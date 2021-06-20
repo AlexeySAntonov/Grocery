298c3fba52483fbf458e4f31b086a1cc0acdb92d
Alexey Antonov <alex.antonov150@gmail.com>
2021-06-21 00:21:46 +0300

    TrolleyListInteractor tests.

--- feature-trolleylist-impl/src/test/java/TrolleyListInteractorTest.kt
+++ feature-trolleylist-impl/src/test/java/TrolleyListInteractorTest.kt
@@ -1,3 +1,4 @@
+import com.aleksejantonov.core.model.SyncStatus
 import com.aleksejantonov.core.test.MainCoroutineScopeRule
 import com.aleksejantonov.core.ui.base.adapter.delegate.RemoveAllItem
 import com.aleksejantonov.feature.trolleylist.impl.business.TrolleyListInteractor
@@ -5,6 +6,7 @@ import com.aleksejantonov.feature.trolleylist.impl.business.TrolleyListInteracto
 import com.aleksejantonov.feature.trolleylist.impl.data.TrolleyListRepository
 import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
@@ -38,5 +40,64 @@ class TrolleyListInteractorTest {
     val items = trolleyListInteractor.data().first()
     assert(items.size == TrolleysFactory.nonEmptyData().size + 1) // + Remove all item
     assert(items.last() is RemoveAllItem)
+  }
+
+  @Test
+  fun `create trolley`() = runBlocking {
+    coEvery { repositoryMock.createTrolley("Name", "Description") } returns 1L
+    val id = trolleyListInteractor.createTrolley("Name", "Description")
+    assert(id == 1L)
+  }
+
+  // Proxy
+  @Test
+  fun `delete trolley`() = runBlocking {
+    val id = 1L
+    coEvery { repositoryMock.deleteTrolley(id) } returns Unit
+    val result = trolleyListInteractor.deleteTrolley(id)
+    assert(result == Unit)
+  }
+
+  // Proxy
+  @Test
+  fun `delete all trolleys`() = runBlocking {
+    coEvery { repositoryMock.deleteAllTrolleys() } returns Unit
+    val result = trolleyListInteractor.deleteAllTrolleys()
+    assert(result == Unit)
+  }
+
+  // Proxy
+  @Test
+  fun `sync trolley`() = runBlocking {
+    val id = 1L
+    coEvery { repositoryMock.syncTrolley(id) } returns Unit
+    val result = trolleyListInteractor.syncTrolley(id)
+    assert(result == Unit)
+  }
+
+  @Test
+  fun `sync remote data`() = runBlocking {
+    val flow = flow<SyncStatus> {
+      emit(SyncStatus.UPDATING)
+      delay(10L)
+      emit(SyncStatus.FAILED)
+      delay(10L)
+      emit(SyncStatus.UPDATING)
+      delay(10L)
+      emit(SyncStatus.DONE)
+    }
+    coEvery { repositoryMock.syncRemoteData() } returns flow
+    val data = trolleyListInteractor.syncRemoteData()
+    val firstStatus = data.first()
+    assert(firstStatus == SyncStatus.UPDATING)
+    coroutineScope.advanceTimeBy(10L)
+    val secondStatus = data.drop(1).first()
+    assert(secondStatus == SyncStatus.FAILED)
+    coroutineScope.advanceTimeBy(10L)
+    val thirdStatus = data.drop(2).first()
+    assert(thirdStatus == SyncStatus.UPDATING)
+    coroutineScope.advanceTimeBy(10L)
+    val fourthStatus = data.drop(3).first()
+    assert(fourthStatus == SyncStatus.DONE)
   }
 }
